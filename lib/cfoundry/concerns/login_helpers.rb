4ce96801350de10c9dc1ec29a89ee17edb157a56
Eduardo Garcia Cebollero <kiwnix@gmail.com>
2013-05-29 23:51:59 +0200

    Allow working with systems without UAA enabled

--- lib/cfoundry/concerns/login_helpers.rb
+++ lib/cfoundry/concerns/login_helpers.rb
@@ -3,11 +3,23 @@ require "base64"
 module CFoundry
   module LoginHelpers
     def login_prompts
-      @base.uaa.prompts
+      if (!@base.uaa.nil?)
+        @base.uaa.prompts
+      else
+           {
+              :password => ["password" , "Password"],
+              :username => ["text","Email" ]
+            }
+      end
     end
 
     def login(username, password)
-      @base.token = AuthToken.from_uaa_token_info(@base.uaa.authorize(username, password))
+      if (!@base.uaa.nil?)
+        @base.token = AuthToken.from_uaa_token_info(@base.uaa.authorize(username, password))
+      else
+            resp = MultiJson.load(@base.post("users",username,"tokens", :content => :json, :payload => '{"password":"'+password+'"}'))
+            @base.token = AuthToken.new(resp['token'],nil)
+      end
     end
   end
 end
