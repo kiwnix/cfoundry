dc0be8aab6318a3adfb84cd7394afca9f0b5d4a0
Eduardo Garcia Cebollero <kiwnix@gmail.com>
2013-05-29 21:46:34 +0200

    Revert commit 125b200323522d37965ff00434557000348285c3 that removes v1 compatibility

--- spec/cfoundry/client_spec.rb
+++ spec/cfoundry/client_spec.rb
@@ -7,7 +7,13 @@ describe CFoundry::Client do
 
   subject { CFoundry::Client.new('http://example.com') }
 
-  it "returns a v2 client" do
+  it "returns a v1 client when used on a v1 target" do
+    stub_request(:get, "http://example.com/info").to_return(:status => 200, :body => '{"version":1}')
+    subject.should be_a(CFoundry::V1::Client)
+  end
+
+  it "returns a v2 client when used on a v2 target" do
+    stub_request(:get, "http://example.com/info").to_return(:status => 200, :body => '{"version":2}')
     subject.should be_a(CFoundry::V2::Client)
   end
 end
